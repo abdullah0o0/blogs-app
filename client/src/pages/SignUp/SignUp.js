38c5096dd00c5a04eab97e5bdd7f1fbae2a84375
abdullah0o0 <abdullah.syallah0@gmail.com>
2021-07-14 16:20:46 +0200

    add a submitHandler func

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -1,30 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios'
+import baseURL from '../../config/baseURL'
 
 export default function SignUp() {
-    const submitHandler = (e) =>{
-        e.prventDefault()
-        const userName= e.target.username.value;
-        const email = e.target.email.value;
-        const password =e.target.password.value;
-        const confirmPassword= e.target.confirmPassword.value;
+  const [error, setError] = useState();
+  const [success, setSuccess] = useState();
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const userName = e.target.username.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const confirmPassword = e.target.confirmPassword.value;
 
-        const data ={
-            userName,email,password,confirmPassword
-        }
-        console.log('sign up data ==>',data);
+    const data = {
+      userName,
+      email,
+      password,
+      confirmPassword,
+    };
 
-        //POST req  ==> http://localhost:5000/api/v1/signup
+    console.log("Sign up data ==> ", data);
+    try {
+      const res = await axios.post(baseURL + "/users/signup", data);
+      if (res.data.error) {
+        setError(res.data.error);
+        setSuccess(null);
+      } else {
+        setError(null);
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("userId", res.data.userId);
+        // alert('registered successfully')
+        setSuccess("registered successfully, redirect in 3s");
+        setTimeout(() => {
+          window.location.replace("/signin");
+        }, 3000);
+      }
+      console.log("RES ==> ", res.data);
+    } catch (e) {
+      console.log(e);
     }
+    // POST req  ==> http://localhost:5000/api/v1/signup
+  };
   return (
-    <div>
-      <h1>Signup</h1>
+    <div className="container">
+      <h1>Sign Up</h1>
+      {error && (
+        <div className="alert alert-danger" role="alert">
+          {error}
+        </div>
+      )}
+      {success && (
+        <div className="alert alert-success" role="alert">
+          {success}
+        </div>
+      )}
       <form onSubmit={submitHandler}>
-      <div className="mb-3">
+        <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
-            User Name
+            Username
           </label>
           <input
-          name='username'
+            name="username"
             type="text"
             className="form-control"
             id="exampleInputEmail1"
@@ -36,7 +72,7 @@ export default function SignUp() {
             Email address
           </label>
           <input
-            name='email'
+            name="email"
             type="email"
             className="form-control"
             id="exampleInputEmail1"
@@ -51,23 +87,25 @@ export default function SignUp() {
             Password
           </label>
           <input
-          name='password'
+            name="password"
             type="password"
             className="form-control"
             id="exampleInputPassword1"
           />
         </div>
+
         <div className="mb-3">
           <label htmlFor="exampleInputPassword1" className="form-label">
             Confirm Password
           </label>
           <input
-          name='confirmPassword'
+            name="confirmPassword"
             type="password"
             className="form-control"
             id="exampleInputPassword1"
           />
         </div>
+
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
