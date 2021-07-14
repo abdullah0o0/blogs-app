0fdc9ba66cf750a766df45a267e25aacae4636c1
abdullah0o0 <abdullah.syallah0@gmail.com>
2021-07-14 16:22:55 +0200

    add users schema

---
+++ models/Users.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  avatar: {
+    type: String,
+    default:
+      "https://img.favpng.com/25/13/19/samsung-galaxy-a8-a8-user-login-telephone-avatar-png-favpng-dqKEPfX7hPbc6SMVUCteANKwj.jpg",
+  },
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
